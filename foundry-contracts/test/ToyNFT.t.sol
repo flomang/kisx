6126b3ac0912225bbca2a424a196b3e302f0eff2
flo <inbox.733@gmail.com>
2023-05-08 15:19:25 -0600

    Moved remappings to vscode settings.json file. The Toy contract should also support solidity v 0.8.x.

--- foundry-contracts/test/ToyNFT.t.sol
+++ foundry-contracts/test/ToyNFT.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.10;
+pragma solidity ^0.8.10;
 
 import "forge-std/Test.sol";
 import {IERC721Receiver} from "openzeppelin-contracts/contracts/interfaces/IERC721Receiver.sol";
