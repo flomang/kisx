f6181ea1b4c4f32b9485c0ee11c2cc73c43a6537
flo <inbox.733@gmail.com>
2023-05-15 14:33:34 -0600

    The signup page should have a success message on signup success.

--- svelte-frontend/src/routes/signup/+page.svelte
+++ svelte-frontend/src/routes/signup/+page.svelte
@@ -9,6 +9,7 @@
     import { Icon as CommonIcon } from "@smui/common";
     import HelperText from "@smui/textfield/helper-text";
     import Paper, { Title, Subtitle, Content } from "@smui/paper";
+    import { Icon } from "@smui/icon-button";
 
     let username = "";
     let email = "";
@@ -18,6 +19,7 @@
     let usernameError = "";
     let emailError = "";
     let passwordError = "";
+    let success = true;
 
     interface SignupResult {
         signup: {
@@ -87,8 +89,9 @@
 
             const { token } = data?.signup.user ?? {};
             if (token) {
-                addToken(token);
-                goto("/home");
+                success = true;
+                //    addToken(token);
+                //    goto("/home");
             }
         } catch (error: any) {
             message = error.message;
@@ -113,98 +116,117 @@
 </script>
 
 <div class="container">
-    <Paper color="secondary" variant="clear" style="width: 100%;">
-        <Title class="title"><b>Sign Up</b></Title>
-        <Content>
-            <div class="username-container">
-                <Textfield
-                    variant="filled"
-                    style="width: 100%;"
-                    class="input-container"
-                    bind:value={username}
-                    on:input={handleInputUsername}
-                    invalid={usernameError != ""}
-                >
-                    <svelte:fragment slot="label">
-                        <CommonIcon
-                            class="material-icons"
-                            style="font-size: 1em; line-height: normal; vertical-align: top;"
-                            >face</CommonIcon
-                        >
-                        <span>Username</span>
-                    </svelte:fragment>
-                    <HelperText persistent={usernameError != ""} slot="helper"
-                        >{usernameError}</HelperText
+    {#if !success}
+        <Paper color="secondary" variant="clear" style="width: 100%;">
+            <Title class="title"><b>Sign Up</b></Title>
+            <Content>
+                <div class="username-container">
+                    <Textfield
+                        variant="filled"
+                        style="width: 100%;"
+                        class="input-container"
+                        bind:value={username}
+                        on:input={handleInputUsername}
+                        invalid={usernameError != ""}
                     >
-                </Textfield>
-            </div>
-            <div class="email-container">
-                <Textfield
-                    variant="filled"
-                    style="width: 100%;"
-                    class="input-container"
-                    bind:value={email}
-                    on:input={handleInputEmail}
-                    invalid={emailError != ""}
-                >
-                    <svelte:fragment slot="label">
-                        <CommonIcon
-                            class="material-icons"
-                            style="font-size: 1em; line-height: normal; vertical-align: top;"
-                            >email</CommonIcon
+                        <svelte:fragment slot="label">
+                            <CommonIcon
+                                class="material-icons"
+                                style="font-size: 1em; line-height: normal; vertical-align: top;"
+                                >face</CommonIcon
+                            >
+                            <span>Username</span>
+                        </svelte:fragment>
+                        <HelperText
+                            persistent={usernameError != ""}
+                            slot="helper">{usernameError}</HelperText
                         >
-                        <span>Email</span>
-                    </svelte:fragment>
-                    <HelperText
-                        class="helper-text"
-                        persistent={emailError != ""}
-                        slot="helper">{emailError}</HelperText
+                    </Textfield>
+                </div>
+                <div class="email-container">
+                    <Textfield
+                        variant="filled"
+                        style="width: 100%;"
+                        class="input-container"
+                        bind:value={email}
+                        on:input={handleInputEmail}
+                        invalid={emailError != ""}
                     >
-                </Textfield>
-            </div>
-            <div class="password-container">
-                <Textfield
-                    variant="filled"
-                    style="width: 100%;"
-                    bind:value={password}
-                    on:input={handleInputPassword}
-                    type="password"
-                    invalid={passwordError != ""}
-                >
-                    <svelte:fragment slot="label">
-                        <CommonIcon
-                            class="material-icons"
-                            style="font-size: 1em; line-height: normal; vertical-align: top;"
-                            >lock</CommonIcon
+                        <svelte:fragment slot="label">
+                            <CommonIcon
+                                class="material-icons"
+                                style="font-size: 1em; line-height: normal; vertical-align: top;"
+                                >email</CommonIcon
+                            >
+                            <span>Email</span>
+                        </svelte:fragment>
+                        <HelperText
+                            class="helper-text"
+                            persistent={emailError != ""}
+                            slot="helper">{emailError}</HelperText
                         >
-                        <span>Password</span>
-                    </svelte:fragment>
-                    <HelperText slot="helper" persistent={passwordError != ""}
-                        >{passwordError}</HelperText
+                    </Textfield>
+                </div>
+                <div class="password-container">
+                    <Textfield
+                        variant="filled"
+                        style="width: 100%;"
+                        bind:value={password}
+                        on:input={handleInputPassword}
+                        type="password"
+                        invalid={passwordError != ""}
                     >
-                </Textfield>
-            </div>
-            <div class="agree-container">
-                <FormField>
-                    <Checkbox bind:checked={agree} />
-                    Agree to terms and conditions
-                </FormField>
-            </div>
-            <div class="button-container">
-                <Button
-                    on:click={handleSignup}
-                    variant="raised"
-                    style="width: 100%; height: 100%;"
-                    disabled={!is_valid(email, password, username, agree)}
-                >
-                    <Label>Submit</Label>
-                </Button>
-            </div>
-            <div class="links-container">
-                <a href="/login">Already have an account?</a>
-            </div>
-        </Content>
-    </Paper>
+                        <svelte:fragment slot="label">
+                            <CommonIcon
+                                class="material-icons"
+                                style="font-size: 1em; line-height: normal; vertical-align: top;"
+                                >lock</CommonIcon
+                            >
+                            <span>Password</span>
+                        </svelte:fragment>
+                        <HelperText
+                            slot="helper"
+                            persistent={passwordError != ""}
+                            >{passwordError}</HelperText
+                        >
+                    </Textfield>
+                </div>
+                <div class="agree-container">
+                    <FormField>
+                        <Checkbox bind:checked={agree} />
+                        Agree to terms and conditions
+                    </FormField>
+                </div>
+                <div class="button-container">
+                    <Button
+                        on:click={handleSignup}
+                        variant="raised"
+                        style="width: 100%; height: 100%;"
+                        disabled={!is_valid(email, password, username, agree)}
+                    >
+                        <Label>Submit</Label>
+                    </Button>
+                </div>
+                <div class="links-container">
+                    <a href="/login">Already have an account?</a>
+                </div>
+            </Content>
+        </Paper>
+    {:else}
+        <Paper color="secondary" variant="clear" style="width: 100%;">
+            <Title class="title"><b> Thanks for signing up!</b></Title>
+            <Content>
+                <div class="content-container">
+                    <Icon class="material-icons" on>send</Icon>
+                    A verification link has been sent to your email address. Please
+                    verify your email address before continuing.
+                </div>
+                <div class="links-container">
+                    <a href="/login">Back to Login</a>
+                </div>
+            </Content>
+        </Paper>
+    {/if}
 </div>
 
 <style>
@@ -245,6 +267,10 @@
         justify-content: center;
         width: 100%;
         height: 45px;
+        padding-bottom: 16px;
+    }
+
+    .content-container {
         padding-bottom: 16px;
     }
 
