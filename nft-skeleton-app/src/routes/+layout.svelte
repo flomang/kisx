cbc63bd9ea2a2c41ef8bafa5288b1806d0dcf050
flo <inbox.733@gmail.com>
2023-09-15 14:07:44 -0600

    Added logout button.

--- nft-skeleton-app/src/routes/+layout.svelte
+++ nft-skeleton-app/src/routes/+layout.svelte
@@ -1,12 +1,49 @@
 <script lang="ts">
 	import "../app.postcss";
 	import { AppShell, AppBar } from "@skeletonlabs/skeleton";
+	import client, { removeToken } from "../lib/apollo";
 	import { Avatar } from "@skeletonlabs/skeleton";
 	import { page } from "$app/stores";
-	import { Toast, initializeStores } from '@skeletonlabs/skeleton';
+	import { Toast, initializeStores } from "@skeletonlabs/skeleton";
+	import {
+		computePosition,
+		autoUpdate,
+		offset,
+		shift,
+		flip,
+		arrow,
+	} from "@floating-ui/dom";
+	import { popup } from "@skeletonlabs/skeleton";
+	import type { PopupSettings } from "@skeletonlabs/skeleton";
+	import { storePopup } from "@skeletonlabs/skeleton";
+	import { goto } from "$app/navigation";
+	import type { PageData } from "./$types";
+	import Icon from "@iconify/svelte";
 
+	export let data: PageData;
+
+	// init toast store
 	initializeStores();
+
+	storePopup.set({ computePosition, autoUpdate, offset, shift, flip, arrow });
+
 	let ignore = ["/login", "/signup", "/forgot"];
+
+	const popupFeatured: PopupSettings = {
+		// Represents the type of event that opens/closed the popup
+		event: "click",
+		// Matches the data-popup value on your popup element
+		target: "popupFeatured",
+		// Defines which side of your trigger the popup will appear
+		placement: "bottom",
+	};
+
+	const handleSignout = async () => {
+		client.cache.reset();
+		removeToken();
+		goto("/login");
+		data = {};
+	};
 </script>
 
 <!-- App Shell -->
@@ -45,7 +82,18 @@
 					GitHub
 				</a> -->
 					<!-- <Avatar src="https://i.pravatar.cc/" /> -->
-					<Avatar src="invalid-image.jpg" initials="ML" />
+					<button
+						type="button"
+						class="btn-icon btn-icon-sm variant-filled"
+						use:popup={popupFeatured}
+					>
+						<Icon
+							icon="majesticons:user-line"
+							color="gray"
+							width="30"
+							height="30"
+						/></button
+					>
 				</svelte:fragment>
 			</AppBar>
 		{/if}
@@ -55,3 +103,14 @@
 	<!-- Page Route Content -->
 	<slot />
 </AppShell>
+
+<div class="card p-4 max-w-sm" data-popup="popupFeatured">
+	<div class="grid grid-cols-1 gap-2">
+		<div>{data.username}</div>
+		<button
+			type="button"
+			class="btn btn-sm variant-soft-primary"
+			on:click={handleSignout}>logout</button
+		>
+	</div>
+</div>
---
+++ nft-skeleton-app/src/routes/+layout.ts
@@ -0,0 +1,43 @@
+import { browser } from '$app/environment';
+import { redirect } from '@sveltejs/kit';
+import client from "../lib/apollo";
+import { gql } from "@apollo/client/core";
+
+interface GetCurrentUserResult {
+    getCurrentUser: {
+        user: {
+            token: string;
+            username: string;
+        };
+    };
+}
+
+const CURRENT_USER = gql`
+    query GetCurrentUser {
+        getCurrentUser {
+            user {
+                token
+                username
+            }
+        }
+    }
+`;
+
+/** @type {import('./$types').LayoutLoad} */
+export async function load({ route }) {
+    let ignore = ['/login', '/signup', '/forgot'];
+
+    if (browser && route.id && !ignore.includes(route.id)) {
+        try {
+            let {data} =  await client.query<GetCurrentUserResult>({
+                    query: CURRENT_USER,
+            });
+
+            return data?.getCurrentUser.user ?? {};
+            
+        } catch (error: any) {
+            // user is not logged in 
+            throw redirect(307, '/login');
+        }
+    }
+}
