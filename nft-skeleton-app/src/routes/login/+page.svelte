1ed93c0de2397680d717dc3c3da52fe81ce54d26
flo <inbox.733@gmail.com>
2023-09-15 12:01:25 -0600

    Disable login button until valid input

--- nft-skeleton-app/src/routes/login/+page.svelte
+++ nft-skeleton-app/src/routes/login/+page.svelte
@@ -167,9 +167,9 @@
             </div>
             <div>
                 <button
-                    type="submit"
                     on:click={handleSignin}
-                    class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    disabled={!is_valid(email, password)}
+                    class="disabled:cursor-not-allowed disabled:opacity-50 flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                     >Login</button
                 >
             </div>
